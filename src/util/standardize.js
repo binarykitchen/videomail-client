5acaf85dc7e9f1ccfa9c02254716500efea30599
Michael Heuberger <michael.heuberger@binarykitchen.com>
2016-01-23 14:03:34 +1300

    improve shims

--- src/util/standardize.js
+++ src/util/standardize.js
@@ -3,8 +3,8 @@ module.exports = function(window, navigator) {
     require('es5-shim')
     require('cross-class-list')
 
-    if (!navigator)
-        throw new Error('Navigator is missing!')
+    // https://github.com/julienetie/request-frame
+    require('request-frame')('native')
 
     navigator.getUserMedia_ =
         navigator.getUserMedia ||
@@ -12,30 +12,8 @@ module.exports = function(window, navigator) {
         navigator.mozGetUserMedia ||
         navigator.msGetUserMedia
 
-    window.AudioContext =
-        window.AudioContext ||
-        window.webkitAudioContext
-
-    window.URL =
-        window.URL ||
-        window.webkitURL ||
-        window.mozURL ||
-        window.msURL
-
-    window.requestAnimationFrame =
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame
-
-    window.cancelAnimationFrame =
-        window.cancelAnimationFrame ||
-        window.webkitCancelAnimationFrame ||
-        window.webkitCancelRequestAnimationFrame ||
-        window.mozCancelRequestAnimationFrame ||
-        window.oCancelRequestAnimationFrame ||
-        window.msCancelRequestAnimationFrame
+    window.AudioContext = window.AudioContext || window.webkitAudioContext
+    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL
 
     window.XMLHttpRequest = (
         window.XMLHttpRequest || function() {
@@ -59,59 +37,5 @@ module.exports = function(window, navigator) {
         method = methods[length]
 
         if (!console[method]) console[method] = noop
-    }
-
-    if (typeof Object.create != 'function') {
-        Object.create = (function() {
-            var Object = function() {}
-
-            return function (prototype) {
-                Object.prototype = prototype
-                var result = {}
-                Object.prototype = null
-                return result
-            }
-        })()
-    }
-
-    // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-    if (!Object.keys) {
-
-        Object.keys = (function() {
-
-            var hasOwnProperty = Object.prototype.hasOwnProperty,
-                hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-                dontEnums = [
-                    'toString',
-                    'toLocaleString',
-                    'valueOf',
-                    'hasOwnProperty',
-                    'isPrototypeOf',
-                    'propertyIsEnumerable',
-                    'constructor'
-                ],
-                dontEnumsLength = dontEnums.length
-
-            return function(obj) {
-
-                if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-                    throw new TypeError('Object.keys called on non-object')
-                }
-
-                var result = [], prop, i
-
-                for (prop in obj) {
-                    if (hasOwnProperty.call(obj, prop)) result.push(prop)
-                }
-
-                if (hasDontEnumBug) {
-                    for (i = 0; i < dontEnumsLength; i++) {
-                        if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i])
-                    }
-                }
-
-                return result
-            }
-        }())
     }
 }
