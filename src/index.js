f2af74341ed49d4f822644692278f1e21b4a952b
Michael Heuberger <michael.heuberger@binarykitchen.com>
2016-11-15 18:52:32 +1300

    rearrange shim order and bump few packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-var Client = require('./client')
+var standardize = require('./util/standardize'),
+    Client = require('./client')
 
 if (!navigator) {
     throw new Error('Navigator is missing!')
 
 } else {
-    var standardize = require('./util/standardize')
-
     // Ensures Videomail functionality is not broken on exotic browsers with shims.
     //
     // UMD (Universal Module Definition), inspired by https://github.com/es-shims/es5-shim
