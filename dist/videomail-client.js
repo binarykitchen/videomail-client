91c5065ec986ee3beb3075c3dcc18c162f07f175
Michael Heuberger <michael.heuberger@binarykitchen.com>
2017-08-23 16:59:48 +1200

    Final commit of version 2.0.21

--- dist/videomail-client.js
+++ dist/videomail-client.js
@@ -13435,7 +13435,7 @@ function wrappy (fn, cb) {
 },{}],83:[function(_dereq_,module,exports){
 module.exports={
   "name": "videomail-client",
-  "version": "2.0.20",
+  "version": "2.0.21",
   "description": "A wicked npm package to record videos directly in the browser, wohooo!",
   "author": "Michael Heuberger <michael.heuberger@binarykitchen.com>",
   "contributors": [
@@ -18477,6 +18477,9 @@ var Recorder = function Recorder(visuals, replay, options) {
   function writeStream(buffer, opts) {
     if (stream) {
       if (stream.destroyed) {
+        // prevents https://github.com/binarykitchen/videomail.io/issues/393
+        stopPings();
+
         self.emit(_events2.default.ERROR, _videomailError2.default.create('Already disconnected', 'Sorry, the connection to the server has been destroyed. Please reload. ' + 'Details of buffer: ' + buffer.toString(), options));
       } else {
         var onFlushedCallback = opts && opts.onFlushedCallback;
