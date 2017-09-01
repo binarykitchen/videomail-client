a75261af161e5251c1068ba8dffb3cf8d9d09f00
Michael Heuberger <michael.heuberger@binarykitchen.com>
2017-09-02 11:49:27 +1200

    more android fixes

--- src/wrappers/optionsWrapper.js
+++ src/wrappers/optionsWrapper.js
@@ -24,11 +24,21 @@ export default {
     options.getRatio = function () {
       var ratio = 1 // just a default one when no computations are possible
 
-      if (this.hasDefinedDimensions()) {
-        ratio = this.video.height / this.video.width
-
       // todo fix this, it's not really an option
-      } else if (this.videoHeight && this.videoWidth) {
+      const hasVideoDimensions = this.videoHeight && this.videoWidth
+
+      if (this.hasDefinedDimensions()) {
+        if (hasVideoDimensions) {
+          // figure out first which one to pick
+          if (this.videoHeight < this.video.height) {
+            ratio = this.videoHeight / this.videoWidth
+          } else {
+            ratio = this.video.height / this.video.width
+          }
+        } else {
+          ratio = this.video.height / this.video.width
+        }
+      } else if (hasVideoDimensions) {
         ratio = this.videoHeight / this.videoWidth
       }
 
