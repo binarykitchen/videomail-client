5f238e163a658f8d2492eff5f62a22b887378628
Michael Heuberger <michael.heuberger@binarykitchen.com>
2017-09-02 12:19:08 +1200

    Final commit of version 2.1.3

--- dist/videomail-client.js
+++ dist/videomail-client.js
@@ -13435,7 +13435,7 @@ function wrappy (fn, cb) {
 },{}],83:[function(_dereq_,module,exports){
 module.exports={
   "name": "videomail-client",
-  "version": "2.1.2",
+  "version": "2.1.3",
   "description": "A wicked npm package to record videos directly in the browser, wohooo!",
   "author": "Michael Heuberger <michael.heuberger@binarykitchen.com>",
   "contributors": [
@@ -17078,7 +17078,7 @@ exports.default = {
       if (this.hasDefinedDimensions()) {
         if (hasVideoDimensions) {
           // figure out first which one to pick
-          if (this.videoHeight < this.video.height) {
+          if (this.videoHeight < this.video.height || this.videoWidth < this.video.width) {
             ratio = this.videoHeight / this.videoWidth;
           } else {
             ratio = this.video.height / this.video.width;
