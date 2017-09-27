7d1fe0f8dd70872f9f2c49015f1553e2cc5ea470
Michael Heuberger <michael.heuberger@smxemail.com>
2017-09-28 12:12:25 +1300

    improve error msg for firefox on ios

--- src/util/browser.js
+++ src/util/browser.js
@@ -53,16 +53,12 @@ const Browser = function (options) {
     var warning
 
     if (firefox) {
-      warning = 'Probably you need to <a href="' + firefoxDownload + '" target="_blank">' +
-                'upgrade Firefox</a> to fix this.'
-
-      // just temporarily
-      warning += JSON.stringify(self.getUsefulData())
-      warning += '<br/>okBrowser=' + okBrowser
-      warning += '<br/>isOkSafari=' + isOkSafari
-      warning += '<br/>isOkIOS=' + isOkIOS
-      warning += '<br/>isBadIOS=' + isBadIOS
-      warning += '<br/>canRecord=' + self.canRecord()
+      if (isIOS) {
+        warning = 'Firefox on iOS is not ready for webcams yet. Hopefully in near future ...'
+      } else {
+        warning = 'Probably you need to <a href="' + firefoxDownload + '" target="_blank">' +
+                  'upgrade Firefox</a> to fix this.'
+      }
     } else if (isChrome) {
       if (isIOS) {
         warning = 'Chrome on iOS is not ready for webcams yet. Hopefully in near future ...'
