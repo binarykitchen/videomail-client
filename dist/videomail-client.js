6556dc5cc7fcbba5da47482bf1eb4d9561ed7b45
Michael Heuberger <michael.heuberger@binarykitchen.com>
2017-09-28 12:00:45 +1300

    Final commit of version 2.1.8

--- dist/videomail-client.js
+++ dist/videomail-client.js
@@ -13486,7 +13486,7 @@ function wrappy (fn, cb) {
 },{}],83:[function(_dereq_,module,exports){
 module.exports={
   "name": "videomail-client",
-  "version": "2.1.7",
+  "version": "2.1.8",
   "description": "A wicked npm package to record videos directly in the browser, wohooo!",
   "author": "Michael Heuberger <michael.heuberger@binarykitchen.com>",
   "contributors": [
@@ -14425,14 +14425,16 @@ var Browser = function Browser(options) {
 
       // just temporarily
       warning += JSON.stringify(self.getUsefulData());
+      warning += '<br/>okBrowser=' + okBrowser;
+      warning += '<br/>isOkSafari=' + isOkSafari;
+      warning += '<br/>isOkIOS=' + isOkIOS;
+      warning += '<br/>isBadIOS=' + isBadIOS;
+      warning += '<br/>canRecord=' + self.canRecord();
     } else if (isChrome) {
       if (isIOS) {
         warning = 'Chrome on iOS is not ready for webcams yet. Hopefully in near future ...';
       } else {
         warning = 'Probably you need to <a href="' + chromeDownload + '" target="_blank">' + 'upgrade Chrome</a> to fix this.';
-
-        // just temporarily
-        warning += JSON.stringify(self.getUsefulData());
       }
     } else if (isChromium) {
       warning = 'Probably you need to <a href="' + chromiumDownload + '" target="_blank">' + 'upgrade Chromium</a> to fix this.';
