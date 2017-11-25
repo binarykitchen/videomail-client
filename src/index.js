6a7ca44958d2457f73a73a60a2d63f54590ae633
Michael Heuberger <michael.heuberger@binarykitchen.com>
2017-11-25 14:16:21 +1300

    fix examples, make replays stabler and improved entry point

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ if (!navigator) {
   throw new Error('Navigator is missing!')
 } else {
   // Ensures Videomail functionality is not broken on exotic browsers with shims.
-  //
-  // UMD (Universal Module Definition), inspired by https://github.com/es-shims/es5-shim
-  ;(function (navigator) {
-    standardize(window, navigator)
-  }(navigator))
+  standardize(window, navigator)
 }
 
 export default Client
