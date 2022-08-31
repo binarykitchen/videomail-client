345a2cad0526000fe31e211485417d49e6aa3579
Michael Heuberger <michael.heuberger@binarykitchen.com>
2022-08-31 19:06:22 +1200

    Final commit of version 3.0.1

--- prototype/js/videomail-client.js
+++ prototype/js/videomail-client.js
@@ -25842,7 +25842,7 @@ function wrappy (fn, cb) {
 },{}],341:[function(_dereq_,module,exports){
 module.exports={
   "name": "videomail-client",
-  "version": "3.0.0",
+  "version": "3.0.1",
   "description": "A wicked npm package to record videos directly in the browser, wohooo!",
   "author": "Michael Heuberger <michael.heuberger@binarykitchen.com>",
   "contributors": [
