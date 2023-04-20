324f01889e6e82b6caf9843904723db61d62343d
Michael Heuberger <michael.heuberger@binarykitchen.com>
2023-04-20 13:40:19 +1200

    output more

--- env/dev/release.sh
+++ env/dev/release.sh
@@ -52,7 +52,9 @@ git checkout master
 git pull
 git checkout develop
 
+info "Figuring next version ...\n"
 read VERSION <<<$(gulp bumpVersion --importance=$IMPORTANCE | awk '/to/ {print $5}')
+printf "${GREEN}It's ${VERSION}${NC}"
 
 git checkout master
 git push
