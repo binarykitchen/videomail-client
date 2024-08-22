56da3264cd05617cc74b9a76d82d749b7cc2c3fa
Michael Heuberger <michael.heuberger@binarykitchen.com>
2024-08-22 13:16:02 +1200

    bump vc

--- prototype/js/videomail-client.js
+++ prototype/js/videomail-client.js
@@ -17267,7 +17267,7 @@ function wrappy (fn, cb) {
 },{}],116:[function(_dereq_,module,exports){
 module.exports={
   "name": "videomail-client",
-  "version": "9.1.14",
+  "version": "9.1.16",
   "description": "A wicked npm package to record videos directly in the browser, wohooo!",
   "keywords": [
     "webcam",
