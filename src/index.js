137e2c4442c94537653c9ee351fdef317383b078
Michael Heuberger <michael.heuberger@binarykitchen.com>
2024-10-07 14:02:27 +1300

    remove standardize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import Client from "./client";
-import standardize from "./util/standardize";
-
-// Ensures Videomail functionality is not broken on exotic browsers with shims.
-standardize(window, navigator);
 
 export default Client;
