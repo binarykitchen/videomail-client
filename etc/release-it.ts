1856bde2f5dde8321fe3af3b44b4c0c8e6e970dd
Michael Heuberger <michael.heuberger@binarykitchen.com>
2024-10-09 11:17:58 +1300

    only-version

--- etc/release-it.ts
+++ etc/release-it.ts
@@ -1,18 +1,10 @@
 import type { Config } from "release-it";
 
 const config: Config = {
-  git: {
-    commit: true,
-    tag: true,
-    push: true,
-  },
   // TODO Consider later once all is stable
   // github: {
   //   release: true,
-  // },
-  npm: {
-    publish: true,
-  },
+  // }
 };
 
 export default config;
