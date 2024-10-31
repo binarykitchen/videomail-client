d6715079af2c949418b7bdd2e11c714f94718c6c
Michael Heuberger <michael.heuberger@binarykitchen.com>
2024-11-01 11:44:40 +1300

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Client from "./client";
 import VideomailEvents from "./types/events";
+import { PartialVideomailClientOptions } from "./types/options";
 import Videomail from "./types/Videomail";
 
 export type { Videomail };
 export type { VideomailEvents };
+export type { PartialVideomailClientOptions };
 
 export default Client;
