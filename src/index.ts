f2829c839dc8e90de9eb0ba4e1be582ecd760f76
Michael Heuberger <michael.heuberger@binarykitchen.com>
2025-02-16 15:35:20 +1300

    export videomail client options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import VideomailClient from "./client";
 import { FullVideomailErrorData, VideomailErrorData } from "./types/error";
 import VideomailEvents from "./types/events";
+import { VideomailClientOptions } from "./types/options";
 import RecordingStats from "./types/RecordingStats";
 import Videomail from "./types/Videomail";
 import { VideoType } from "./types/VideoType";
@@ -8,5 +9,6 @@ import { VideoType } from "./types/VideoType";
 export type { Videomail };
 export type { VideomailEvents };
 export type { RecordingStats };
+export type { VideomailClientOptions };
 export { VideoType, VideomailClient };
 export type { VideomailErrorData, FullVideomailErrorData };
