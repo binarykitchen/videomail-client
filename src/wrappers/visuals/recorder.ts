6411d9df63a3ecc0ef99307af5ba08b25d9836c9
Michael Heuberger <michael.heuberger@binarykitchen.com>
2025-05-12 19:49:04 +1200

    polish some text

--- src/wrappers/visuals/recorder.ts
+++ src/wrappers/visuals/recorder.ts
@@ -783,7 +783,7 @@ class Recorder extends Despot {
           }
 
           const err = createError({
-            message: "Oh no, websocket server error!",
+            message: "Websocket error from Videomail server",
             explanation,
             err: deserializeError(command.args?.err),
             options: this.options,
