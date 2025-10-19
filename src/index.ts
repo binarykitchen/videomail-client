11c09d46166bdf67fa4fbd811dd92ffeb0ce56b5
Michael Heuberger <michael.heuberger@binarykitchen.com>
2025-10-19 19:38:47 +1300

    Fix delivery record type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import VideomailClient from "./client";
 
 export type { Command } from "./types/command";
-export type DeliveryRecord = "./types/DeliveryRecord";
 export type { VideomailCommandArgs } from "./types/command";
+export type { DeliveryRecord } from "./types/Delivery";
 export type { EmailAddress, EmailAddresses } from "./types/EmailAddress";
 export type { FullVideomailErrorData, VideomailErrorData } from "./types/error";
 export type { VideomailEvents, VideomailPreviewParams } from "./types/events";
