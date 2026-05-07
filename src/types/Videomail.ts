d3d58960fb6d5a0b5c4b73aa6eb3fed254d9afb4
Michael Heuberger <michael.heuberger@binarykitchen.com>
2026-05-07 20:16:48 +1200

    Apply patches and improve typings

--- src/types/Videomail.ts
+++ src/types/Videomail.ts
@@ -11,7 +11,10 @@ import VideoFormat from "./VideoFormat";
 // Remember, only primitive types are supported.
 // LevelDB can't store for example Sets or Maps.
 export interface Videomail {
+  url: string;
+
   // First, typical email typings
+
   subject?: string;
   body?: string;
 
@@ -23,6 +26,22 @@ export interface Videomail {
   cc?: EmailAddresses | undefined;
 
   alias: string;
+
+  replyAllUrl?: string;
+  replyUrl: string;
+
+  // Deliveries
+
+  sentTo?: DeliveryRecord;
+  sentCc?: DeliveryRecord;
+  sentBcc?: DeliveryRecord;
+
+  rejectedTo?: DeliveryRecord;
+  rejectedCc?: DeliveryRecord;
+  rejectedBcc?: DeliveryRecord;
+
+  editUrl?: string;
+  repeatUrl?: string;
 
   // Then Videomail specific stuff
 
@@ -36,8 +55,10 @@ export interface Videomail {
   expiresAfterServerPretty: string;
   format?: VideoFormat;
   height?: number | undefined;
+  width?: number | undefined;
   key: string;
   mp4?: string;
+  webm?: string;
   vtt?: string;
   captions?: string | undefined;
   poster?: string | undefined;
@@ -47,14 +68,28 @@ export interface Videomail {
 
   parentKey?: string | undefined;
 
+  // Now, the social features/types
+
+  userKey?: UserKey | undefined;
+
   reactions?: ReactionsByUserKey | undefined;
   reactionsPretty?: ReactionsPrettyByUserKey | undefined;
 
-  replyAllUrl?: string;
-  replyUrl: string;
+  comment?: {
+    count?: number | undefined;
+    url?: string | undefined;
+  };
 
-  commentUrl?: string | undefined;
   shareUrl?: string | undefined;
+
+  // Never stored. Points to the latest public videomail poster of the user, if any.
+  // Used for the Videomail wall.
+  userPublicPoster?: string | undefined;
+
+  // When true, the videomail is visible to the Videomail wall
+  public?: boolean;
+
+  // Then sending stats
 
   sending: boolean;
   sent?: boolean;
@@ -64,42 +99,20 @@ export interface Videomail {
   sentDateServerPretty?: string;
   serverTimePretty?: string;
 
+  // Whitelist stuff
+
   whitelistKey: string;
 
   // Never stored, for snapshots only and retrieved via whitelist key
   // Consider removing once all has been migrated to the new whitelist system.
   siteName?: string | undefined;
 
-  url: string;
-
-  userKey?: UserKey | undefined;
-
-  // Never stored. Points to the latest public videomail poster of the user, if any.
-  // Used for the Videomail wall.
-  userPublicPoster?: string | undefined;
+  // General info
 
   versions: {
     videomailNinjaFormPlugin?: string;
     videomailClient: string;
   };
-  webm?: string;
-  width?: number | undefined;
-
-  // Deliveries
-
-  sentTo?: DeliveryRecord;
-  sentCc?: DeliveryRecord;
-  sentBcc?: DeliveryRecord;
-
-  rejectedTo?: DeliveryRecord;
-  rejectedCc?: DeliveryRecord;
-  rejectedBcc?: DeliveryRecord;
-
-  // When true, the videomail is visible to the Videomail wall
-  public?: boolean;
-
-  editUrl?: string;
-  repeatUrl?: string;
 }
 
 export type PartialVideomail = PartialDeep<Videomail>;
